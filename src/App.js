1060386652d1f45f4bdd7479b6fb83d360774f8c
devMahmoud <eng.mahmoud.mohamed93@gmail.com>
2022-05-25 17:50:18 +0200

    Convert App component to class component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+/* eslint-disable */
+import { Component } from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
